c46766cabd5b27494743606668ee268370309cd1
chelsiprajapati <chelsiprajapati97@gmail.com>
2022-09-28 19:29:58 +0530

    minor changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,9 +39,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "8ac07d973b8f9d58e6efee80ca1a5f4b",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "4b50598c779bdccaa157852b83ffdaae",
-"/": "4b50598c779bdccaa157852b83ffdaae",
-"main.dart.js": "dd47d7bb77849ce611b4fac235fbb211",
+"index.html": "2f097e469e22a36f233331f425efe458",
+"/": "2f097e469e22a36f233331f425efe458",
+"main.dart.js": "cc4149d7582dcc44daff11c40f0a85ca",
 "manifest.json": "4734db19b089f1a4d7b37fdc9824bcd2",
 "version.json": "e995cedc6c83665a9c4cbe7541bbf600"
 };
